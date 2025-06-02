4b33805fb4d9e39267e8cc286c203f265e146b8d
Jay Oswald <jayoswald@catalyst-au.net>
2025-06-02 12:38:56 +1000

    fix namespace for privacy provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace report_completionoverview;
+namespace report_completionoverview\privacy;
 
 class provider implements
     // This plugin does not store any personal user data.
